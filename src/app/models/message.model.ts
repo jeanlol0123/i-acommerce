5ad0664172bd92e7c536cbf46f6a80248b6eb815
Jeanlol <jeanpaulol0123@gmail.com>
2024-04-26 11:31:25 -0500

    arreglo "lorem"

--- src/app/models/message.model.ts
+++ src/app/models/message.model.ts
@@ -1,12 +1,4 @@
-export class Message{
-    sender: string
+export interface Message{
+    sender: string,
     content: string | any
-
-
-    constructor(sender:string, content:string){
-        this.sender = sender;
-        this.content = content;
-    }
-
-
 }
