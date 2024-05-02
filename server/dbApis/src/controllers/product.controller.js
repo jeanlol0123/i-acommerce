32ba0a155ff4a8e3d004893877b275d112bff5bb
Disruptor <davidsantiagojimenez16@gmail.com>
2024-05-02 12:17:33 -0500

    Se realiza la primera implementacion de API de las personas

---
+++ server/dbApis/src/controllers/product.controller.js
@@ -0,0 +1,19 @@
+export const getProduct = (req, res) => {
+    res.send('Obteniendo usuarios');
+}
+
+export const getSingleProduct = (req, res) => {
+    res.send('Obteniendo un usuario');  
+}
+
+export const postProduct  = (req, res) => {
+    res.send('Creando usuarios');
+}
+
+export const deleteProduct = (req, res) => {
+    res.send('Borrando Usuarios');
+}
+
+export const updateProduct = (req, res) => {
+    res.send('Actualizando Usuarios');
+}
