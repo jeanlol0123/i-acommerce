0af5d5418da2ff9dbbb3e8e3f1726a9aad458c09
Disruptor <davidsantiagojimenez16@gmail.com>
2024-05-27 01:04:17 -0500

    ola cors desactivao

--- server/dbApis/main.js
+++ server/dbApis/main.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import {PORT} from './config.js';
-import cors from 'cors';
+//import cors from 'cors';
 import userRoutes from './src/routes/person.routes.js';
 import productRoutes from './src/routes/product.routes.js';
 import shipmentRoutes from './src/routes/shipment.routes.js';
@@ -9,11 +9,11 @@ import orderDetailRoutes from './src/routes/orderDetail.routes.js';
 import wholeInvoiceRoute from './src/routes/getWholeInvoice.routes.js';
 import ping from './src/routes/ping.js';
 const app = express();
-
+/*
 app.use(cors({
     origin: 'https://gradually-aware-scorpion.ngrok-free.app'
 }));
-  
+*/
 
 app.use(express.json())
 app.use(ping);
